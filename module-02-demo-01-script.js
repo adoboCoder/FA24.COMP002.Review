b8ab30a7a16d512f23079ac422c8d1696bb8c25e
adobocoder <jcruz@adobo.dev>
2024-10-23 21:53:06 -0700

    first commit

---
+++ module-02-demo-01-script.js
@@ -0,0 +1,41 @@
+// Lecture Activity: Values, Types, and Operators
+
+/*
+*   Operators:
+*       Add (+)
+*       Subtract (-)
+*       Multiply (*)
+*       Divide (/)
+*       Modulo or Remainder (%)
+*
+*   Comparison Operators:
+*       Equals (==)
+*       Not-Equal (!=)
+*       Greater Than (>)
+*       Less Than (<)
+*       Greater Than Or Equal (>=)
+*       Less Than Or Equal (<=)
+*   
+*   Logical Operators:
+*       And (&&)
+*       Or (||)
+*       Ternary (?:)
+*/
+
+
+//     8 * null = ???
+console.log(8 * null); // output: 0
+//     “5” - 1 = ???
+console.log("5" - 1); // output: 4
+//     “5” + 1 = ???
+console.log("5" + 1); // output: 51
+//     “five” * 2 = ???
+console.log("five" * 2); // output: NaN
+//     false == 0 = ???
+console.log(false == 0); // output: true
+//     5 > 2 && 6 > 3 = ???
+console.log(5 > 2 && 6 > 3); // output: true
+//     false == false ? “equal” : “not equal” = ???
+console.log(false == false ? "equal" : "not equal"); // output: equal
+//     true || false = ???
+console.log(true || false); // output: true
